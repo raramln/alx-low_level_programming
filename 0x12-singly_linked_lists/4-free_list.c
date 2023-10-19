360587cbbce250fe53e0a154163400b36528bb3e
raramln <raramelanin01@gmail.com>
2023-10-19 16:28:16 -0700

    list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,12 +9,12 @@
 
 void free_list(list_t *head)
 {
- list_t *current_node;
+list_t *current_node;
 
- while ((current_node = head) != NULL)
- {
- head = head->next;
- free(current_node->str);
- free(current_node);
- }
+while ((current_node = head) != NULL)
+{
+head = head->next;
+free(current_node->str);
+free(current_node);
+}
 }
